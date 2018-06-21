7946bf83ed574a516d5d95d25f7692269a5bcd2f
Moody "Kuuy" Wizmann <mail.kuuy@gmail.com>
2018-06-21 14:29:54 +0800

    Create main.go

---
+++ 180621/Fingerprints/main.go
@@ -0,0 +1,34 @@
+// 注意：在CF平台下，Go语言的scanf需要处理行尾的"\n"
+
+package main
+
+import "fmt"
+
+func main() {
+    n := 0
+    m := 0
+
+    fmt.Scanf("%d%d\n", &n, &m)
+
+    ns := make([]int, n)
+    ms := make([]int, 10)
+
+    for i := 0; i < n; i++ { 
+        fmt.Scanf("%d", &ns[i])
+    }
+    fmt.Scanf("\n")
+
+    for i := 0; i < m; i++ {
+        x := 0
+        fmt.Scanf("%d", &x)
+        ms[x] = 1
+    }
+    fmt.Scanf("\n")
+
+    for i := 0; i < n; i++ {
+        if (ms[ns[i]] != 0) {
+            fmt.Printf("%d ", ns[i])
+        }
+    }
+    fmt.Println("")
+}
